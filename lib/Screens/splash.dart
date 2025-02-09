def7d2ee49dd25d9813a99158191726c7b5ae2ef
Parasjeet Singh <singhparasjeet01@gmail.com>
2025-02-09 13:49:06 +0530

    images added and error build solved

---
+++ lib/Screens/splash.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Center(
+        child: Container(
+          height: 100,
+            width: 100,
+            child: Text(''))));
+  }
+}
